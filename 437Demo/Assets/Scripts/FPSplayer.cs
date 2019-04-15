a3e47b47b1f9df6b9438a89e72b2a693ed29988f
abk7997 <=>
2019-04-15 18:53:47 -0500

    Upgraded to some much better FPS controls.

--- 437Demo/Assets/Scripts/FPSplayer.cs
+++ 437Demo/Assets/Scripts/FPSplayer.cs
@@ -7,37 +7,47 @@ public class FPSplayer : MonoBehaviour
 {
     public float movementSpeedModifier;
     public float rotationSpeedModifier;
+    public float diagonalSpeedInhibiter;
     private Rigidbody rb;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        Cursor.visible = false; //turn of mouse cursor
     }
 
     // Update is called once per frame
     void Update()
     {
+        bool twoWay = false;
+
         //Front and Back
         if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
         {
             rb.velocity = transform.forward * movementSpeedModifier;
+            twoWay = true;
         }
 
-        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
+        else if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
         {
             rb.velocity = -transform.forward * movementSpeedModifier;
+            twoWay = true;
         }
 
         //Turn left/right
         if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(new Vector3(0, 1, 0) * rotationSpeedModifier, Space.World);
+            //transform.Rotate(new Vector3(0, 1, 0) * rotationSpeedModifier, Space.World);
+            if (!twoWay) rb.velocity = transform.right * movementSpeedModifier;
+            else rb.velocity += transform.right * movementSpeedModifier * diagonalSpeedInhibiter;
         }
 
-        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        else if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
         {
-            transform.Rotate(new Vector3(0, -1, 0) * rotationSpeedModifier, Space.World);
+            //transform.Rotate(new Vector3(0, -1, 0) * rotationSpeedModifier, Space.World);
+            if (!twoWay) rb.velocity = -transform.right * movementSpeedModifier;
+            else rb.velocity += -transform.right * movementSpeedModifier * diagonalSpeedInhibiter;
         }
 
         //Jump
